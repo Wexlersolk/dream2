b9c010b6a6641f9aa16769bc2ad19ae61e9df8a2
Wexler763 <danylkarp@gmail.com>
2024-10-25 10:34:46 +0300

    feat: dream init

--- cmd/dream.go
+++ cmd/dream.go
@@ -1,1 +1,19 @@
-//establish dream config
+package main
+
+import (
+	"errors"
+	"time"
+)
+
+type Dream struct {
+	score int
+	day   time.Time
+	tasks []string
+}
+
+func NewDream(score int, day time.Time, tasks []string) (*Dream, error) {
+	if score < 0 || score > 10 {
+		return nil, errors.New("score must be between 0 and 10")
+	}
+	return &Dream{score: score, day: day, tasks: tasks}, nil
+}
